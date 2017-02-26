9df4a5f26e5608b11ec3b09ea9b8c1ee9336f1db
Michael Brooks <me@michaelbrooks.co.uk>
2017-02-26 22:54:19 +0000

    Installed foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} =  require('react-router');
 var Main = require('Main');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main} >
